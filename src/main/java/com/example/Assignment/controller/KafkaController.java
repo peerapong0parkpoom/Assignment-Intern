ec0b4bbec61db9857ea74b7a99b00c52f1dd6956
peerapong0parkpoom <peerapong.parkpoom@gmail.com>
2023-06-26 14:58:42 +0700

    question 13

---
+++ src/main/java/com/example/Assignment/controller/KafkaController.java
@@ -0,0 +1,24 @@
+package com.example.Assignment.controller;
+
+import com.example.Assignment.service.KafkaProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kafka")
+public class KafkaController {
+    private final KafkaProducerService kafkaProducerService;
+
+    @Autowired
+    public KafkaController(KafkaProducerService kafkaProducerService) {
+        this.kafkaProducerService = kafkaProducerService;
+    }
+
+    @PostMapping("/send")
+    public void sendMessage(@RequestBody String message) {
+        kafkaProducerService.sendMessage(message);
+    }
+}
